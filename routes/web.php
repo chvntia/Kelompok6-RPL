1f89b0d426db5956251b9657a86d09372cb990eb
Chyntia Oktavia Zulkifli <chyntiaoktavia2410@gmail.com>
2024-05-08 14:41:15 +0700

    Chyntia Sprint 2

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\userController;
+use App\Http\Controllers\beritaController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,5 +18,8 @@ Route::middleware([
     })->name('dashboard');
     Route::get('/anggota',[userController::class, 'index'])->name('user.index');
     Route::get('/anggota/approval',[userController::class, 'approval'])->name('user.approval');
-
+    Route::get('/berita/add',[beritaController::class, 'index'])->name('berita.add');
+    Route::get('/berita/approval',[beritaController::class, 'approval'])->name('berita.approval');
+    Route::get('/berita',[beritaController::class, 'show'])->name('berita.show');
+    
 });
