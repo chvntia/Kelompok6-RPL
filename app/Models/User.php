eb97766e7d7eb0531dd17f3518014134d694b0f2
Chyntia Oktavia Zulkifli <chyntiaoktavia2410@gmail.com>
2024-05-09 22:29:25 +0700

    Chyntia Sprint 2

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,21 +54,4 @@ class User extends Authenticatable
         'profile_photo_url',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
-
-    public function Berita()
-    {
-        return $this->hasMany(Berita::class);
-    }
 }
