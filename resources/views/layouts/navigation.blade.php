fb753de9f5d0a408781ee3ce0dad752227e5d7ee
Chyntia Oktavia Zulkifli <chyntiaoktavia2410@gmail.com>
2024-05-12 20:32:34 +0700

    Update Chyntia

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,5 +1,5 @@
-<aside class="z-20 hidden w-64 overflow-y-auto bg-white md:block flex-shrink-0 border bg-lime-50">
-    <div class="py-4 font-bold">
+<aside class="z-20 hidden w-64 overflow-y-auto bg-white md:block flex-shrink-0 border">
+    <div class="py-4">
         <a class="ml-6 text-lg font-bold" href="{{ route('dashboard') }}">
             Selamat Datang!
         </a>
@@ -11,31 +11,31 @@
                 </x-nav-link>
             </li>
 
-            <li class="relative px-6 py-3 rounded-md transition duration-300 ease-in-out hover:bg-lime-200 {{ request()->routeIs('user.index') ? 'bg-lime-100 rounded-md' : '' }}">
+            <li class="relative px-6 py-3 rounded-md transition duration-300 ease-in-out hover:bg-lime-200 {{ request()->routeIs('user.index') ? 'bg-lime-200 rounded-md' : '' }}">
                 <x-nav-link href="{{ route('user.index') }}">
                     {{ __('Tambah Pengguna') }}
                 </x-nav-link>
             </li>
 
-            <li class="relative px-6 py-3 rounded-md transition duration-300 ease-in-out hover:bg-lime-200 {{ request()->routeIs('user.approval') ? 'bg-lime-100 rounded-md' : '' }}">
+            <li class="relative px-6 py-3 rounded-md transition duration-300 ease-in-out hover:bg-lime-200 {{ request()->routeIs('user.approval') ? 'bg-lime-200 rounded-md' : '' }}">
                 <x-nav-link href="{{ route('user.approval') }}">
                     {{ __('Approval Pengguna') }}
                 </x-nav-link>
             </li>
             
-            <li class="relative px-6 py-3 rounded-md transition duration-300 ease-in-out hover:bg-lime-200 {{ request()->routeIs('user.approval') ? 'bg-lime-100 rounded-md' : '' }}">
+            <li class="relative px-6 py-3 rounded-md transition duration-300 ease-in-out hover:bg-lime-200 {{ request()->routeIs('berita.show') ? 'bg-lime-200 rounded-md' : '' }}">
                 <x-nav-link href="{{ route('berita.show') }}">
                     {{ __('Berita') }}
                 </x-nav-link>
             </li>
-            <li class="relative px-6 py-3 rounded-md transition duration-300 ease-in-out hover:bg-lime-200 {{ request()->routeIs('user.approval') ? 'bg-lime-100 rounded-md' : '' }}">
+            <li class="relative px-6 py-3 rounded-md transition duration-300 ease-in-out hover:bg-lime-200 {{ request()->routeIs('berita.add') ? 'bg-lime-200 rounded-md' : '' }}">
                 <x-nav-link href="{{ route('berita.add') }}">
                     {{ __('Add Berita') }}
                 </x-nav-link>
             </li>   
 
             
-            <li class="relative px-6 py-3 rounded-md transition duration-300 ease-in-out hover:bg-lime-200 {{ request()->routeIs('user.approval') ? 'bg-lime-100 rounded-md' : '' }}">
+            <li class="relative px-6 py-3 rounded-md transition duration-300 ease-in-out hover:bg-lime-200 {{ request()->routeIs('berita.approval') ? 'bg-lime-200 rounded-md' : '' }}">
                 <x-nav-link href="{{ route('berita.approval') }}">
                     {{ __('Berita Approval') }}
                 </x-nav-link>
