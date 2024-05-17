5adffc7a4842827e8755851564f7b0f1e25b0590
Chyntia Oktavia Zulkifli <chyntiaoktavia2410@gmail.com>
2024-05-17 12:03:16 +0700

    Update Chyntia

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\userController;
 use App\Http\Controllers\beritaController;
+use App\Http\Controllers\desaController;
+use App\Http\Controllers\layananController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,5 +23,12 @@ Route::middleware([
     Route::get('/berita/add',[beritaController::class, 'index'])->name('berita.add');
     Route::get('/berita/approval',[beritaController::class, 'approval'])->name('berita.approval');
     Route::get('/berita',[beritaController::class, 'show'])->name('berita.show');
+    Route::get('/berita/update/{id}',[beritaController::class, 'update'])->name('berita.update');
+    Route::get('/desa/kegiatan',[desaController::class, 'kegiatan'])->name('desa.kegiatan');
+    Route::get('/desa/approval',[desaController::class, 'approval'])->name('desa.approval');
+    Route::get('/desa/show',[desaController::class, 'show'])->name('desa.show');
+    Route::get('/layanan/add',[layananController::class, 'add'])->name('layanan.add');
+    Route::get('/layanan/approval',[layananController::class, 'approval'])->name('layanan.approval');
+    Route::get('/layanan/show',[layananController::class, 'show'])->name('layanan.show');
     
 });
