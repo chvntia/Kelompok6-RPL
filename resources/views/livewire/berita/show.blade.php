f76df4dae53024a15531da38af3eb508626b5147
fairuzichsan <fairuzichsan@student.telkomuniversity.ac.id>
2024-05-26 11:19:11 +0700

    sprint 3 irus

---
+++ resources/views/livewire/berita/show.blade.php
@@ -0,0 +1,44 @@
+<section>
+    <div class="px-4 py-6 mx-auto md:px-12 lg:px-6 max-w-7xl">
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 text-sm font-medium text-gray-500 text-balance">
+            @foreach ($berita as $berita)
+            <div class="border rounded-xl">
+                <div class="p-12 overflow-hidden">
+                    <img src="{{asset('storage/'.$berita->path_image_berita)}}" alt="#_" width="480" height="480" class="w-full h-full mx-auto">
+                </div>
+                <div class="flex flex-col justify-between px-4 mt-4 overflow-hidden">
+                    <div>
+                        <p class="text-gray-900 text-base">{{$berita->judul}}</p>
+                        <p class="text-gray-900 text-xs text-base">{{$berita->kategori}}</p>
+                        <p class="text-gray-900 text-xs text-base">{{$berita->link}}</p>
+                        <p class="mt-2">{{$berita->deskripsi}}</p>
+                    </div>
+                    <div class="flex mt-8">
+                        <!-- <a href="#" class="mb-5 inline-flex items-center justify-center text-sm font-semibold text-black duration-200 hover:text-blue-500 focus:outline-none" aria-label="Read more about the topic">
+                            <span>Read more</span>
+                            <svg aria-hidden="true" focusable="false" class="size-4 ml-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7l5 5m0 0l-5 5m5-5H6"></path>
+                            </svg>
+                        </a> -->
+                        @can('admin')
+                        <a href="{{ route('berita.update',$berita->id) }}" class="mb-5 ml-4 inline-flex items-center justify-center text-sm font-semibold text-black duration-200 hover:text-red-500 focus:outline-none">
+                            <span class="mr-2">Edit</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                <path d="M20 14.66V20a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h5.34"></path>
+                                <polygon points="18 2 22 6 12 16 8 16 8 12 18 2"></polygon>
+                            </svg>
+                        </a>
+                        <button wire:click="deleteBerita({{ $berita->id }})" class="mb-5 ml-4 inline-flex items-center justify-center text-sm font-semibold text-black duration-200 hover:text-red-500 focus:outline-none" aria-label="Delete news">
+                            <span>Delete</span>
+                            <svg aria-hidden="true" focusable="false" class="size-4 ml-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
+                            </svg>
+                        </button>
+                        @endcan
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</section>
---
+++ resources/views/livewire/layanan/add.blade.php
@@ -0,0 +1,31 @@
+<div class="container mx-auto py-8">
+    <h1 class="text-xl font-bold mb-4 text-center">Request Layanan atau Fasilitas</h1>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <form enctype="multipart/form-data" class="max-w-md mx-auto bg-white p-6 rounded-md shadow-md">
+        <div class="mb-4">
+            <label for="layanan" class="block text-gray-700 font-bold mb-2">Layanan</label>
+            <select id="layanan" name="layanan" wire:model="layanan" class="w-full border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-400" required>
+                <option value="" hidden>Pilih Layanan atau Fasilitas</option>
+                <option value="lapangan basket">Lapangan Basket</option>
+                <option value="lapangan bola">Lapangan Bola</option>
+                <option value="gedung serba guna">Gedung Serba Guna</option>
+                <option value="billiard">Billiard</option>
+            </select>
+        </div>
+        <div class="mb-4">
+            <label for="keterangan" class="block text-gray-700 font-bold mb-2">Keterangan</label>
+            <textarea id="keterangan" name="keterangan" wire:model="keterangan" class="w-full border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-400" placeholder="Masukan Alasan Request Layanan atau Fasilitas" rows="4" required></textarea>
+        </div>
+        <div class="flex justify-end">
+            <button type="button" wire:click="addLayanan" data-confirm-delete class="bg-lime-500 hover:bg-lime-600 text-white font-bold py-2 px-4 rounded">Submit</button>
+        </div>
+    </form>
+</div>
